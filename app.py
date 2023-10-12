4784715f8b1b3c550659e728d664f5c59f06dc79
Nitsofts Inc <100211678+nitsofts@users.noreply.github.com>
2023-10-12 17:57:13 +1100

    Added cdsc fetch data.py

--- app.py
+++ app.py
@@ -1,59 +1,43 @@
 from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
-@app.route('/add', methods=['GET', 'POST'])
-def add_numbers():
-    if request.method == 'GET':
-        num1 = float(request.args.get('num1', 0))
-        num2 = float(request.args.get('num2', 0))
-    elif request.method == 'POST':
-        data = request.get_json()
-        num1 = float(data.get('num1', 0))
-        num2 = float(data.get('num2', 0))
+# Helper function to scrape and format the data
+def scrape_cdsc_data():
+    # Define the URL of the website
+    url = "https://www.cdsc.com.np/"
 
-    result = num1 + num2
-    response = {'result': result}
+    # Send an HTTP request to the website with SSL certificate verification disabled
+    response = requests.get(url, verify=False)
 
-    return jsonify(response)
+    # Parse the HTML content of the website
+    html_content = response.text
+    soup = BeautifulSoup(html_content, "html.parser")
 
-@app.route('/buy', methods=['GET'])
-def buy():
-    units = float(request.args.get('units', 0))
-    buying_price = float(request.args.get('buying_price', 0))
+    # Locate the div containing the "Current Public Issue" information
+    div = soup.find("div", class_="fun-factor-area")
 
-    share_amount = round(units * buying_price, 2)
-    sebon_fee = round((0.015 / 100) * share_amount, 2)
+    # Extract all the "h4" elements from the "fun-custom-column" div
+    h4_elements = div.find_all("h4")
 
-    if share_amount <= 50000:
-        broker_commission = round(max(0.004 * share_amount, 10), 2)
-    elif 50001 <= share_amount <= 500000:
-        broker_commission = round(0.0037 * share_amount, 2)
-    elif 500001 <= share_amount <= 2000000:
-        broker_commission = round(0.0034 * share_amount, 2)
-    elif 2000001 <= share_amount <= 10000000:
-        broker_commission = round(0.003 * share_amount, 2)
-    else:
-        broker_commission = round(0.0027 * share_amount, 2)
+    # Create a list to store the extracted data with swapped positions
+    data = []
 
-    dp_charge = 25
-    total_paying_amount = round(share_amount + sebon_fee + broker_commission + dp_charge, 2)
-    cost_per_share = round(total_paying_amount / units, 2)
-    price_per_share = round(buying_price + (broker_commission / units), 2)
-    total_charges = round(sebon_fee + broker_commission + dp_charge, 2)
+    # Swap the positions of the "h4" elements and add them to the list
+    for i in range(0, len(h4_elements), 2):
+        data.append({h4_elements[i + 1].text.strip(): h4_elements[i].text.strip()})
 
+    return data
 
-    response_dict = [{
-        'Share Amount': share_amount,
-        'Sebon Fee': sebon_fee,
-        'Broker Commission': broker_commission,
-        'DP Charge': dp_charge,
-        'Price Per Share': price_per_share,
-        'Total Charges': total_charges,
-        'Payable Amount': total_paying_amount
-    }]
+@app.route('/get_cdsc_data', methods=['GET'])
+def get_cdsc_data():
+    # Call the helper function to scrape the data
+    cdsc_data = scrape_cdsc_data()
 
-    return jsonify(response_dict)
+    # Return the scraped data in JSON format
+    return jsonify(cdsc_data)
 
-if __name__ == '__main__':
+if __name__ == '__main':
     app.run(host='0.0.0.0', port=8080)
